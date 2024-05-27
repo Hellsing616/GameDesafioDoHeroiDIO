3a1d6fc290946abf3f45caaf2dd8bb7fc0e9efa7
Hellsing616 <lex_info@terra.com.br>
2024-05-27 13:00:12 -0300

    Calcula partidas rankeadas

---
+++ js/CalculadoraPartidasRankeadas.js
@@ -0,0 +1,28 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O jogador tem um saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso da função
+const resultado = calcularNivel(60, 20);
+console.log(resultado); // Output: O jogador tem um saldo de 40 está no nível de Ouro
+
+calcularNivel(10,3)
